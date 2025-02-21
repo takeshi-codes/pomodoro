df1a68f05b7b22bd9a50454531f58ace06e9182b
CtrlAltDylan <dylangreen.dev@gmail.com>
2025-02-21 09:19:02 -0700

    feat: :sparkles: pomodoro timer
    pomodoro timer complete with chimes signaling cycles and cycle counts

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import tomato from "/tomato.svg";
+import tamato from "/tamato.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(1500);
+  const [workCycles, setWorkCyles] = useState(0);
+  const [breakCycles, setBreakCycles] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [isFirstTimer, setIsFirstTimer] = useState(true);
+
+  const [playBreakFx] = useSound("/break.wav", {
+    volume: 0.25,
+  });
+
+  const [playWorkFx] = useSound("/work.wav", {
+    volume: 0.25,
+  });
+
+  const [playBigBreakFx] = useSound("/big_break.wav", {
+    volume: 0.25,
+  });
+
+  useEffect(() => {
+    if (!isRunning) return;
+    if (time === 0) {
+      if (isFirstTimer) {
+        setWorkCyles((workCycles) => workCycles + 1);
+        if (breakCycles % 3 === 0 && breakCycles !== 0) {
+          playBigBreakFx();
+        } else {
+          playBreakFx();
+        }
+      } else {
+        setBreakCycles((breakCycles) => breakCycles + 1);
+        playWorkFx();
+      }
+
+      if (breakCycles % 3 === 0 && breakCycles !== 0) {
+        setIsFirstTimer((prev) => !prev);
+        setTime(1800);
+      } else {
+        setIsFirstTimer((prev) => !prev);
+        setTime(isFirstTimer ? 300 : 1500);
+      }
+    }
+
+    const interval = setInterval(() => {
+      setTime((prevTime) => (prevTime > 0 ? prevTime - 1 : prevTime));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [time, isRunning, isFirstTimer]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={tamato} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>
+        {`${Math.floor(time / 60)}`.padStart(2, 0)}:
+        {`${time % 60}`.padStart(2, 0)}
+      </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setIsRunning((prev) => !prev)}>
+          {isRunning ? "Pause" : "Start"} Timer
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Works: {workCycles}</p>
+      <p>Breaks: {breakCycles}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
