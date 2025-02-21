c82bdffc1b09983e170c8561461b4adfbb5eaf92
CtrlAltDylan <dylangreen.dev@gmail.com>
2025-02-21 09:44:07 -0700

    build: :zap: #JustMoarBuildTingz

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://takeshi-codes.github.io/pomodoro/",
   plugins: [react()],
   server: {
     watch: {
